a64a87368c3fb25e8108b00d22a1a01a426fcdb9
DC Electronics <48794887+DCelectronics@users.noreply.github.com>
2019-07-08 21:11:53 -0400

    Arduino library compatible with Serial_LCD_Driver

---
+++ LiquidCrystal595/examples/LCD595simple/LCD595simple.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal595.h>    // include the library
+LiquidCrystal595 lcd(2,3,4);     // datapin, latchpin, clockpin
+
+void setup() {
+    
+    
+    lcd.begin(16,2);             // 16 characters, 2 rows
+    lcd.setLED1Pin(HIGH);
+    lcd.setLED2Pin(HIGH);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Hello");
+    lcd.setCursor(0,1);
+    lcd.print("World");
+}
+
+void loop() {
+    // not used.
+}
